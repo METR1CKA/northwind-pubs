66ef8efece3eefcde2fb9c04bf5728ba4c823485
METR1CKA <ferchosalazar054@gmail.com>
2024-05-30 21:12:26 -0600

    [FEAT] (Queries): clients added

---
+++ northwind-queries/cliente.sql
@@ -0,0 +1,103 @@
+-- Active: 1714881146400@@127.0.0.1@3306@northwind
+USE northwind;
+
+------------------------------------------------------------
+# Crear una vista que muestre todas los empleados region
+DROP VIEW IF EXISTS EmployeeRegion;
+
+CREATE VIEW EmployeeRegion AS
+SELECT DISTINCT
+    em.`EmployeeID`,
+    reg.`RegionDescription`
+FROM
+    Employees AS em
+    JOIN EmployeeTerritories AS et ON em.`EmployeeID` = et.`EmployeeID`
+    JOIN Territories AS ter ON et.`TerritoryID` = ter.`TerritoryID`
+    JOIN Region AS reg ON ter.`RegionID` = reg.`RegionID`;
+
+SELECT * FROM EmployeeRegion;
+------------------------------------------------------------
+
+------------------------------------------------------------
+# Crear una vista que muestre todas las ganancias de los clientes
+DROP VIEW IF EXISTS CustomerPurchases;
+
+CREATE VIEW CustomerPurchases AS
+SELECT
+    cus.`CustomerID`,
+    cus.`ContactName`,
+    cus.`CompanyName`,
+    YEAR(ord.`OrderDate`) AS OrderYear,
+    er.`RegionDescription`,
+    SUM(
+        od.`Quantity` * od.`UnitPrice`
+    ) AS TotalPurchase
+FROM
+    Customers AS cus
+    INNER JOIN Orders AS ord ON cus.`CustomerID` = ord.`CustomerID`
+    INNER JOIN `Order Details` AS od ON ord.`OrderID` = od.`OrderID`
+    INNER JOIN EmployeeRegion AS er ON er.`EmployeeID` = ord.`EmployeeID`
+GROUP BY
+    cus.`CustomerID`,
+    cus.`CompanyName`,
+    YEAR(ord.`OrderDate`),
+    er.`RegionDescription`;
+
+SELECT * FROM CustomerPurchases;
+------------------------------------------------------------
+
+------------------------------------------------------------
+# Crear la vista de las compras máximas por año y región
+DROP VIEW IF EXISTS MaxPurchases;
+
+CREATE VIEW MaxPurchases AS
+SELECT
+    OrderYear,
+    RegionDescription,
+    MAX(TotalPurchase) AS MaxTotalPurchase
+FROM CustomerPurchases
+GROUP BY
+    OrderYear,
+    RegionDescription;
+
+SELECT * FROM MaxPurchases;
+------------------------------------------------------------
+
+------------------------------------------------------------
+# Mostrar una tabla que contenga los clientes que comparon mas por año y por region
+SELECT cp.`RegionDescription` AS 'REGION', cp.`OrderYear` AS 'AÑO', cp.`ContactName` AS 'CLIENTE', cp.`CompanyName` AS 'COMPAÑIA', cp.`TotalPurchase` AS 'TOTAL'
+FROM
+    CustomerPurchases AS cp
+    INNER JOIN MaxPurchases AS mp ON cp.`OrderYear` = mp.`OrderYear`
+    AND cp.`RegionDescription` = mp.`RegionDescription`
+    AND cp.`TotalPurchase` = mp.`MaxTotalPurchase`
+ORDER BY cp.`OrderYear`, cp.`RegionDescription`;
+------------------------------------------------------------
+
+------------------------------------------------------------
+# Mostrar una tabla que contenga los clientes que compraron más por año y por región
+SELECT cp.OrderYear AS 'AÑO', GROUP_CONCAT(
+        CASE
+            WHEN cp.`RegionDescription` = 'Northern' THEN CONCAT(cp.`ContactName`)
+        END SEPARATOR ', '
+    ) AS 'Norte', GROUP_CONCAT(
+        CASE
+            WHEN cp.`RegionDescription` = 'Southern' THEN CONCAT(cp.`ContactName`)
+        END SEPARATOR ', '
+    ) AS 'Sur', GROUP_CONCAT(
+        CASE
+            WHEN cp.`RegionDescription` = 'Eastern' THEN CONCAT(cp.`ContactName`)
+        END SEPARATOR ', '
+    ) AS 'Este', GROUP_CONCAT(
+        CASE
+            WHEN cp.`RegionDescription` = 'Westerns' THEN CONCAT(cp.`ContactName`)
+        END SEPARATOR ', '
+    ) AS 'Oeste'
+FROM
+    CustomerPurchases AS cp
+    INNER JOIN MaxPurchases AS mp ON cp.`OrderYear` = mp.`OrderYear`
+    AND cp.`RegionDescription` = mp.`RegionDescription`
+    AND cp.`TotalPurchase` = mp.`MaxTotalPurchase`
+GROUP BY
+    cp.`OrderYear`;
+------------------------------------------------------------
